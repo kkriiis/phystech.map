4b4ba8b9009bb5cfbc24c3c8d8947f5bc9873491
Кристина <m_krissstina@mail.ru>
2022-05-25 12:23:52 +0300

    correct order flags

--- DONE/to_Sonya.sh
+++ DONE/to_Sonya.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd Sonya
-g++ main.cpp hash.cpp Rooms.cpp path.cpp exceptions.cpp -o -Werror -Wall -Wextra test -std=c++17
+g++ main.cpp hash.cpp Rooms.cpp path.cpp exceptions.cpp -Werror -Wall -Wextra -o test -std=c++17
 ./test
-
